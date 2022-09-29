4023f2bd68adb9bd1f1cb584d2879b15724196a1
Maxime Coriton <maxime.coriton@gmail.com>
2022-09-29 14:01:40 +0200

    🚀 RELEASE: nuxt 3

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <NuxtLayout />
+    <NuxtErrorBoundary />
+    <ClientOnly />
+    <ServerPlaceholder />
+    <NuxtLink />
+    <NuxtLoadingIndicator />
+    <LazyNuxtWelcome />
+    <LazyNuxtLayout />
+    <LazyNuxtErrorBoundary />
+    <LazyClientOnly />
+    <LazyServerPlaceholder />
+    <LazyNuxtLink />
+    <LazyNuxtLoadingIndicator />
+  </div>
+</template>
+test
